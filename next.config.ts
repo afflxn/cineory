772f50296d3f0abfc1b4967d3822f9ae06fa5db7
Fyodor <glassyfingernails@gmail.com>
2025-08-25 22:29:53 +0300

    unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
 			16, 32, 48, 64, 96, 128, 256, 384, 512, 640, 750, 828, 1080, 1200, 1920,
 			2048,
 		],
+		unoptimized: true,
 	},
 	experimental: {
 		globalNotFound: true,
